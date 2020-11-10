8c35936064852e292d1205a888e7df7e8b3b6ace
TadashiLee <584638605@qq.com>
2020-11-10 20:53:56 +0800

    [wenchang.li]:finish practice 3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,13 @@
-export const getPoetry
+export const getPoetry = () => {
+  return fetch("https://v1.jinrishici.com/all.json")
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then((data) => {
+      const { origin, author, content } = data;
+      return [origin, author, content];
+    });
+};
